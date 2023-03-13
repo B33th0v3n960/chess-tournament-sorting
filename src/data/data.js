d718833c2bf57beb8fae6efde3c3c71724ee5f56
B33th0v3n960 <zhousun06@gmail.com>
2023-03-13 18:47:13 +0000

    Added js data for rendering

---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+class Profile {
+  #win = 0
+  #loss = 0
+  #draw = 0
+  #point = this.#win + this.#draw / 2
+
+  constructor(name) {
+    this.name = name
+  }
+
+  getWin() {
+    return this.#win
+  }
+  incWin() {
+    this.#win += 1
+    this.#point = this.#win + this.#draw / 2
+  }
+
+  getDraw() {
+    return this.#draw
+  }
+  incDraw(amout) {
+    this.#draw += 1
+    this.#point = this.#win + this.#draw / 2
+  }
+
+  getLoss(){
+    return this.#loss
+  }
+  incLoss() {
+    this.#loss += 1
+  }
+
+  getPoint() {
+    return this.#point
+  }
+}
+
+export default Profile
--- src/react/group.jsx
+++ src/react/group.jsx
@@ -1,19 +1,17 @@
 import '../css/card.css'
 import '../css/group.css'
 import Card from './profile-card'
+import Profile from '../data/data.js'
 
+const player1 = new Profile("Me")
 
 function CardGroup() {
   return (
     <div className='card__group'>
+      <h2>Group Name</h2>
+
       <Card
-        profile={{ name: 'Zhou Sun', point: 3, win: 1, loss: 2, draw: 3 }}
-      />
-      <Card
-        profile={{ name: 'Zhou Sun', point: 3, win: 1, loss: 2, draw: 3 }}
-      />
-      <Card
-        profile={{ name: 'Zhou Sun', point: 3, win: 1, loss: 2, draw: 3 }}
+        profile={player1}
       />
     </div>
   )
