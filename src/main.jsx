44eb33dc7a07054ed14b60fcac2cc012a41a5157
B33th0v3n960 <zhousun06@gmail.com>
2023-03-16 18:43:48 +0000

    Remove extra import function and other component just for better testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import './css/index.css'
+
 import Navbar from './react/navbar'
-import Card from './react/profile-card'
-import CardGroup from './react/group'
-import Profile from './data/data.js'
-import Input from './react/input'
-
-const player1 = new Profile('Me')
-const player2 = new Profile('You')
-const player3 = new Profile('He')
-const list = [player1, player2, player3]
-const groupList = [list, list, list]
-
-const groupListComponent = groupList.map((item, index) => (
-  <CardGroup key={`group-${index}`} list={item} />
-))
+import NameForm from './react/form'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-
-    <div className='group__container'>{groupListComponent}</div>
-
-    <Input count={4} />
+    <NameForm />
   </React.StrictMode>
 )
