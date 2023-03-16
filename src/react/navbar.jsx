b8191b4fe700e6902cb4f3fff425b117ed503950
B33th0v3n960 <zhousun06@gmail.com>
2023-03-16 18:37:57 +0000

    formattor wnat single quote

--- src/react/navbar.jsx
+++ src/react/navbar.jsx
@@ -1,11 +1,41 @@
 import '../css/navbar.css'
+import { useState } from 'react'
 
 function Navbar() {
+  const [sort, setSort] = useState(0)
+
+  function toggleSort() {
+    if (sort === 1) setSort(0)
+    else setSort(1)
+  }
+
   return (
-    <div className="navbar">
+    <div className='navbar'>
       <nav>Chess Tournament Sorting</nav>
+
+      {sort ? <SortBtn /> : <GenerateBtn />}
+
+      <button className='navbar__btn' type='submit'>
+        Clear All
+      </button>
     </div>
   )
+
+  function SortBtn() {
+    return (
+      <button onClick={toggleSort} className='navbar__btn' type='submit'>
+        Sort
+      </button>
+    )
+  }
+
+  function GenerateBtn() {
+    return (
+      <button onClick={toggleSort} className='navbar__btn' type='submit'>
+        Generate
+      </button>
+    )
+  }
 }
 
 export default Navbar
