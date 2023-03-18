86bb5bd5fe8d5e7e0260348d69c6a5b59d4f18e3
B33th0v3n960 <zhousun06@gmail.com>
2023-03-18 12:02:44 +0000

    used form field to generate name input

--- src/react/form.jsx
+++ src/react/form.jsx
@@ -1,22 +1,63 @@
+import { useState, useEffect } from 'react'
 import '../css/form.css'
 import { Input, InputGroup } from './input'
 import range from '../data/range'
 
 function NameForm() {
-  const nameInput = range(1, 4).map((item) => (
-    <div className='group'>
+  const [groupCount, setGroupCount] = useState(0)
+  const [memberCount, setMemberCount] = useState(0)
+
+  async function getGroupCount() {
+    const group = document.querySelector('#group-count').value
+    const member = document.querySelector('#member-count').value
+    if (group && member) {
+      setMemberCount(Number(member))
+      setGroupCount(Number(group))
+    }
+  }
+  useEffect(
+    () => console.log(groupCount, memberCount),
+    [groupCount, memberCount]
+  )
+
+  function GlobalForm() {
+    return (
+      <div className='global-form'>
+        <Input
+          inputName={'group-count'}
+          label={'Number of Groups:'}
+          assignKey={'group-count'}
+          assignId={'group-count'}
+        />
+
+        <Input
+          inputName={'member-count'}
+          label={'Members Per Group:'}
+          assignKey={'member-count'}
+          assignId={'member-count'}
+        />
+
+        <button onClick={getGroupCount} className='navbar__btn' type='submit'>
+          Create Input Fields
+        </button>
+      </div>
+    )
+  }
+
+  const nameInput = range(1, groupCount + 1).map((item) => (
+    <div key={`group-name-${item}`} className='group'>
       <h2>Group {item}</h2>
 
       <div className='bg-sapphire mb-1'>
         <Input
           inputName={`group-${item}`}
           label={'Group Name:'}
-          key={`group-${item}`}
+          assignKey={`group-${item}`}
           mb={0}
         />
       </div>
 
-      <InputGroup count={4} />
+      <InputGroup count={memberCount + 1} />
     </div>
   ))
 
@@ -27,28 +68,6 @@ function NameForm() {
       <form action='' className='name-form'>
         {nameInput}
       </form>
-    </div>
-  )
-}
-
-function GlobalForm() {
-  return (
-    <div className='global-form'>
-      <Input
-        inputName={'group-count'}
-        label={'Number of Groups:'}
-        key={'group-count'}
-      />
-
-      <Input
-        inputName={'member-count'}
-        label={'Members Per Group:'}
-        key={'member-count'}
-      />
-
-      <button className='navbar__btn' type='submit'>
-        Create Input Fields
-      </button>
     </div>
   )
 }
--- src/react/input.jsx
+++ src/react/input.jsx
@@ -14,13 +14,18 @@ export function InputGroup({ count }) {
   return <div>{output}</div>
 }
 
-export function Input({ inputName, label, key, mb = 1 }) {
+export function Input({ inputName, label, assignKey, assignId, mb = 1 }) {
   return (
     <div className={`mb-${mb}`}>
-      <label key={key} htmlFor={inputName} className='input__label'>
+      <label key={assignKey} htmlFor={inputName} className='input__label'>
         {label}
       </label>
-      <input type='text' name={inputName} className='name__input ' />
+      <input
+        id={assignId}
+        type='text'
+        name={inputName}
+        className='name__input '
+      />
     </div>
   )
 }
