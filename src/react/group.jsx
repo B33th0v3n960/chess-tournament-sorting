fa0bad0e1b0e689ebe3fe475cd7d3aab479faac0
B33th0v3n960 <zhousun06@gmail.com>
2023-03-18 14:59:27 +0000

    change the title of group card component

--- src/react/group.jsx
+++ src/react/group.jsx
@@ -1,13 +1,13 @@
 import '../css/card.css'
 import Card from './profile-card'
 
-function CardGroup({ list }) {
+function CardGroup({ list, groupName = 'Group Name' }) {
   const createCards = list.map((item) => (
     <Card key={item.name} profile={item} />
   ))
   return (
     <div className='group'>
-      <h2>Group Name</h2>
+      <h2>{groupName}</h2>
       {createCards}
     </div>
   )
