4684513cecdd119c1fbac7af3d4dd0999977450e
B33th0v3n960 <zhousun06@gmail.com>
2023-03-18 14:56:00 +0000

    removed extra divs

--- src/react/input.jsx
+++ src/react/input.jsx
@@ -1,17 +1,15 @@
 import '../css/form.css'
 import range from '../data/range'
 
-export function InputGroup({ count }) {
+export function InputGroup({ count, assignId }) {
   const output = range(1, count).map((item) => (
-    <div key={`player-name-${item}`} className='mb-1'>
-      <Input
-        inputName={`player-${item}`}
-        label={'Name:'}
-        key={`player-${item}`}
-      />
-    </div>
+    <Input
+      inputName={`player-${item}`}
+      label={'Name:'}
+      key={`player-${item}`}
+    />
   ))
-  return <div>{output}</div>
+  return <div id={assignId}>{output}</div>
 }
 
 export function Input({ inputName, label, assignKey, assignId, mb = 1 }) {
